1c9d4507c584df0ba34667849a8034962436a24e
Googleplex <yyoung2001@gmail.com>
2020-01-25 22:49:21 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    #Send a message when the command /start is issued.
+    update.message.reply_text('Hi!')
+def ping(update, context):
+    update.message.reply_text('pong!')
+def echo(update, context):
+    #Echo the user message.
+    update.message.reply_text(update.message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    TOKEN = "***REMOVED***"
+    NAME = "The name of your app on Heroku"
+    # Port is given by Heroku
+    PORT = os.environ['PORT']
+
+    # Set up the Updater
+    updater = Updater(TOKEN, use_context=True)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('ping', ping))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Webhook mode
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://***REMOVED***.herokuapp.com/{}".format(NAME, TOKEN))
+    # Polling mode
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
