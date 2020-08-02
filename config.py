bb2fdb7c730f05e6ddd5023630280df6bf333f78
Googleplex <yyoung2001@gmail.com>
2020-08-02 17:20:24 +0800

    fix: security issue of eval

--- config.py
+++ config.py
@@ -1,24 +1,25 @@
-import os
+from ast import literal_eval
+from os import environ
 import logging
 
-ENV = os.environ.get('ENV', 'production')
-TOKEN = os.environ.get('TOKEN')
+ENV = environ.get('ENV', 'production')
+TOKEN = environ.get('TOKEN')
 # Webhook url, eg: https://xxx.herokuapp.com/, should end with '/'
-WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
+WEBHOOK_URL = environ.get('WEBHOOK_URL')
 # Port is given by Heroku
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = int(environ.get('PORT', '8443'))
 
-STORAGE = eval(os.environ.get('STORAGE', '[]'))
+STORAGE = literal_eval(environ.get('STORAGE', '[]'))
 DOWNLOAD_DIR = './downloads/' # Requires a slash on the end
-STORAGE_DIR = os.environ.get('STORAGE_DIR', 'Pictures')
+STORAGE_DIR = environ.get('STORAGE_DIR', 'Pictures')
 
-DATABASE = os.environ.get('DATABASE', 'Local')
+DATABASE = environ.get('DATABASE', 'Local')
 # nazurin data collection in database
 NAZURIN_DATA = 'nazurin'
 
-ALBUM_ID = int(os.environ.get('ALBUM_ID'))
-GALLERY_ID = int(os.environ.get('GALLERY_ID'))
-ADMIN_ID = int(os.environ.get('ADMIN_ID'))
+ALBUM_ID = int(environ.get('ALBUM_ID'))
+GALLERY_ID = int(environ.get('GALLERY_ID'))
+ADMIN_ID = int(environ.get('ADMIN_ID'))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
