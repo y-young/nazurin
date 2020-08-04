c9ca6707cdda3fd3e8d8f1b3e7d5f92a2835f073
Googleplex <yyoung2001@gmail.com>
2020-08-04 12:32:39 +0800

    enhance: support more type of  bilibili links

--- sites/Bilibili/interface.py
+++ sites/Bilibili/interface.py
@@ -3,7 +3,10 @@ PRIORITY = 4
 
 patterns = [
     # https://t.bilibili.com/123456789012345678
-    r't\.bilibili\.com/(\d+)'
+    r't\.bilibili\.com/(\d+)',
+
+    # https://t.bilibili.com/h5/dynamic/detail/123456789012345678
+    r't\.bilibili\.com/h5/dynamic/detail/(\d+)'
 ]
 
 def handle(match, **kwargs):
