351aef744b20d186b7fd8cc2103974b2818d13d2
Googleplex <yyoung2001@gmail.com>
2020-08-18 15:23:08 +0800

    feat: support lolibooru

--- sites/Moebooru/interface.py
+++ sites/Moebooru/interface.py
@@ -12,7 +12,13 @@ patterns = [
     r'(konachan\.com)/post/show/(\d+)',
 
     # https://konachan.com/image/1234567890abcdef1234567890abcdef/Konachan.com%20-%20123456%20aaa-bbb%20ccc_ddd%20ddd%20eee.jpg
-    r'(?:[^.]+\.)?(konachan\.com)/(?:image|jpeg|sample)/[a-f0-9]{32}/Konachan\.com%20-%20(\d+)'
+    r'(?:[^.]+\.)?(konachan\.com)/(?:image|jpeg|sample)/[a-f0-9]{32}/Konachan\.com%20-%20(\d+)',
+
+    # https://lolibooru.moe/post/show/123456
+    r'(lolibooru\.moe)/post/show/(\d+)',
+
+    # https://lolibooru.moe/image/1234567890abcdef1234567890abcdef/lolibooru%20123456%20aaa-bbb%20ccc_ddd%20ddd%20eee.jpg
+    r'(lolibooru\.moe)/(?:image|jpeg|sample)/[a-f0-9]{32}/lolibooru%20(\d+)'
 ]
 
 def handle(match, **kwargs):
