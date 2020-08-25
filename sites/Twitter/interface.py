fb05072e52b032c28243a94de99e3430b8fe4632
Googleplex <yyoung2001@gmail.com>
2020-08-25 14:50:39 +0800

    enhance: twitter url matching

--- sites/Twitter/interface.py
+++ sites/Twitter/interface.py
@@ -7,8 +7,9 @@ PRIORITY = 5
 patterns = [
     # https://twitter.com/i/web/status/1234567890123456789
     # https://twitter.com/abcdefg/status/1234567890123456789
+    # https://www.twitter.com/abcdefg/status/1234567890123456789
     # https://mobile.twitter.com/abcdefg/status/1234567890123456789
-    r'(?:mobile\.)?twitter\.com/[^.]+/status/(\d+)'
+    r'(?:mobile\.|www\.)?twitter\.com/[^.]+/status/(\d+)'
 ]
 
 def handle(match, **kwargs):
