703e80c31add6ed575f43f32fd644eb968b14eca
Googleplex <yyoung2001@gmail.com>
2020-08-27 16:28:06 +0800

    enhance: bilibili & twitter plugin api

--- sites/Bilibili/api.py
+++ sites/Bilibili/api.py
@@ -4,11 +4,22 @@ import os
 from utils import downloadImages
 
 class Bilibili(object):
-    def fetch(self, dynamic_id):
-        """Fetch images and detail."""
+    def getDynamic(self, dynamic_id):
+        """Get dynamic data from API."""
         api = 'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id=' + str(dynamic_id)
         source = requests.get(api).json()
         card = json.loads(source['data']['card']['card'])
+        return card
+
+    def fetch(self, dynamic_id):
+        """Fetch images and detail."""
+        card = self.getDynamic(dynamic_id)
+        imgs = self.getImages(card, dynamic_id)
+        downloadImages(imgs)
+        return imgs, card
+
+    def getImages(self, card, dynamic_id):
+        """Get all images in a dynamic card."""
         pics = card['item']['pictures']
         imgs = list()
         for index, pic in enumerate(pics):
@@ -16,6 +27,4 @@ class Bilibili(object):
             basename = os.path.basename(url)
             extension = os.path.splitext(basename)[1]
             imgs.append({'name': str(dynamic_id) + '_' + str(index) + extension, 'url': url})
-
-        downloadImages(imgs)
-        return imgs, card
+        return imgs
--- sites/Twitter/api.py
+++ sites/Twitter/api.py
@@ -3,24 +3,33 @@ import os
 from utils import downloadImages, NazurinError
 
 class Twitter(object):
-    def fetch(self, status_id):
-        """Fetch & return tweet images and information."""
+    def getTweet(self, status_id):
+        """Get a tweet from API."""
         # Old: 'https://syndication.twitter.com/tweets.json?ids='+ status_id +'&lang=en'
         api = 'https://cdn.syndication.twimg.com/tweet?id='+ str(status_id) +'&lang=en'
         response = requests.get(api)
         if response.status_code == 404:
             raise NazurinError('Tweet not found or unavailable.')
-        response = response.json()
-        if 'photos' not in response.keys():
-            raise NazurinError('No photo found.')
+        tweet = response.json()
+        return tweet
 
-        photos = response['photos']
+    def fetch(self, status_id):
+        """Fetch & return tweet images and information."""
+        tweet = self.getTweet(status_id)
+        imgs = self.getImages(tweet)
+        downloadImages(imgs)
+        return imgs, tweet
+
+    def getImages(self, tweet):
+        """Get all images in a tweet."""
+        if 'photos' not in tweet.keys():
+            raise NazurinError('No photo found.')
+        photos = tweet['photos']
         imgs = list()
         for photo in photos:
             filename, url = self.parseUrl(photo['url'])
-            imgs.append({'name': 'twitter - ' + str(status_id) + ' - ' + filename, 'url': url})
-        downloadImages(imgs)
-        return imgs, response
+            imgs.append({'name': 'twitter - ' + tweet['id_str'] + ' - ' + filename, 'url': url})
+        return imgs
 
     def parseUrl(self, src):
         """Get filename & the url of the original image
