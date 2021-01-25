61dbb4b18ed9be6ad5895a5268bd275e7f36a01e
weremexii <weremexii@outlook.com>
2021-01-25 15:43:09 +0800

    update onedrive token cache

--- storage/onedrive.py
+++ storage/onedrive.py
@@ -55,7 +55,10 @@ class OneDrive(object):
         # Update refresh token
         auth_api = msal.ClientApplication(OD_CLIENT,OD_SECRET)
         refresh_token = auth_api.acquire_token_by_refresh_token(refresh_token,["https://graph.microsoft.com/.default"])
-        self.document.update(refresh_token)
+        if self.initialize:
+            self.collection.insert(OD_DOCUMENT,refresh_token)
+        else:
+            self.document.update(refresh_token)
         logger.info('OneDrive refresh token cached')
         
     def store(self, files):
