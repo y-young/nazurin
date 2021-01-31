16c9bc2a61ff4288002366f3e40f4de2a7e8c733
Googleplex <yyoung2001@gmail.com>
2021-01-31 23:13:39 +0800

    enhance(OneDrive): cache access_token & folder_id

--- storage/onedrive.py
+++ storage/onedrive.py
@@ -1,104 +1,137 @@
+import time
 from os import environ
+
+import requests
+
 from config import NAZURIN_DATA, STORAGE_DIR
 from database import Database
 from utils import logger
-import requests
 
 OD_FOLDER = STORAGE_DIR
 OD_CLIENT = environ.get('OD_CLIENT')
 OD_SECRET = environ.get('OD_SECRET')
-OD_RF_TOKEN = environ.get('OD_RF_TOKEN', None) # Refresh token for the first auth
-
+OD_RF_TOKEN = environ.get('OD_RF_TOKEN',
+                          None)  # Refresh token for first-time auth
 OD_DOCUMENT = 'onedrive'
-
-AUTH_URL = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 
 class OneDrive(object):
     """Onedrive driver."""
     db = Database().driver()
     collection = db.collection(NAZURIN_DATA)
     document = collection.document(OD_DOCUMENT)
-    initialize = False
 
+    access_token = None
+    refresh_token = None
+    expires_at = 0
     folder_id = None
 
-    def __init__(self):
-        self.auth()
-        # To find the folder and its id
-        url = 'https://graph.microsoft.com/v1.0/me/drive/root/children' # Here only list the root's children
+    def store(self, files):
+        # https://docs.microsoft.com/zh-cn/graph/api/driveitem-put-content?view=graph-rest-1.0&tabs=http
+        # https://docs.microsoft.com/zh-cn/graph/api/driveitem-createuploadsession?view=graph-rest-1.0
+        self.requireAuth()
+        if not self.folder_id:
+            self.getDestination()
+
+        for item in files:
+            # decorate upload api url
+            url = 'https://graph.microsoft.com/v1.0/me/drive/items/{parent_id}:/{filename}:/content'.format(
+                parent_id=self.folder_id, filename=item.name)
+            with open(item.path, mode='rb') as file:
+                self._request('PUT', url, data=file)
+
+    def findFolder(self, name):
+        self.requireAuth()
         # https://docs.microsoft.com/zh-cn/graph/api/driveitem-list-children?view=graph-rest-1.0&tabs=http
+        url = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
         folders = dict(self._request('GET', url))
         folders = folders.get('value')
         for folder in folders:
-            if folder['name'] == OD_FOLDER:
-                self.folder_id = folder['id']
-                break
-        else:
-            # create a folder
-            body = {"name": OD_FOLDER, "folder": {}}
-            result = self._request('POST', url, json=body)
-            if result.get('id'):
-                self.folder_id = result['id']
+            if folder['name'] == name:
+                return folder['id']
+        return None
 
-    def store(self, files):
-        self.auth()
-        for item in files:
-            # decorate upload api url
-            url = 'https://graph.microsoft.com/v1.0/me/drive/items/{parent_id}:/{filename}:/content'.format(parent_id=self.folder_id, filename=item.name)
-            # https://docs.microsoft.com/zh-cn/graph/api/driveitem-put-content?view=graph-rest-1.0&tabs=http
-            file = open(item.path, mode='rb')
-            self._request('PUT', url, data=file)
-            file.close()
+    def createFolder(self, name):
+        self.requireAuth()
+        # https://docs.microsoft.com/zh-cn/graph/api/driveitem-post-children?view=graph-rest-1.0&tabs=http
+        url = 'https://graph.microsoft.com/v1.0/me/drive/root/children'
+        body = {"name": name, "folder": {}}
+        result = self._request('POST', url, json=body)
+        return result['id']
 
-    def auth(self):
-        # Get a refresh_token from database
-        token_dict = self.document.get()
-        if token_dict:
-            self.initialize = True # To decide whether to insert a document
-            refresh_token = token_dict['refresh_token']
-        else:
-            if OD_RF_TOKEN:
-                refresh_token = OD_RF_TOKEN
-            else:
+    def requireAuth(self):
+        # https://docs.microsoft.com/zh-cn/azure/active-directory/develop/v2-oauth2-auth-code-flow
+        if self.access_token and self.expires_at > time.time():
+            # Logged in, access_token not expired
+            return
+
+        credentials = self.document.get()
+        if credentials:
+            self.refresh_token = credentials['refresh_token']
+            if 'folder_id' in credentials.keys():
+                self.folder_id = credentials['folder_id']
+            if credentials['expires_at'] > time.time():
+                self.access_token = credentials['access_token']
+                self.expires_at = credentials['expires_at']
+                logger.info('OneDrive logged in through cached tokens')
                 return
-        # Make a request
+            else:
+                self.auth()  # Refresh access_token
+        else:
+            # Database should be initialized
+            self.refresh_token = OD_RF_TOKEN
+            self.auth(initialize=True)
+
+    def auth(self, initialize=False):
+        # https://docs.microsoft.com/zh-cn/azure/active-directory/develop/v2-oauth2-auth-code-flow
+        url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
         data = {
             'client_id': OD_CLIENT,
             'client_secret': OD_SECRET,
-            # 'redirect_url': OD_RD_URL,
-            'refresh_token': refresh_token,
+            'refresh_token': self.refresh_token,
             'grant_type': 'refresh_token'
         }
-        response = requests.post(AUTH_URL, data=data)
+        response = requests.post(url, data=data)
         response = self._parse(response)
-        if response.get('access_token'):
-            # Set access token
-            self.token = response['access_token']
+        self.access_token = response['access_token']
+        self.refresh_token = response['refresh_token']
+        self.expires_at = time.time() + response['expires_in']
+        credentials = {
+            'access_token': response['access_token'],
+            'refresh_token': response['refresh_token'],
+            'expires_at': self.expires_at
+        }
+        if initialize:
+            self.collection.insert(OD_DOCUMENT, credentials)
             logger.info('OneDrive logged in')
-            # Update refresh token
-            refresh_token = response['refresh_token']
-            if self.initialize:
-                self.document.update({'refresh_token': refresh_token})
-            else:
-                self.collection.insert(OD_DOCUMENT, {'refresh_token': refresh_token})
-                self.initialize = True
-            logger.info('OneDrive refresh token cached')
+        else:
+            self.document.update(credentials)
+            logger.info('OneDrive access token updated')
+
+    def getDestination(self):
+        # Try to find the folder and its id
+        self.folder_id = self.findFolder(OD_FOLDER)
+        # Not found, create a new folder
+        if not self.folder_id:
+            self.folder_id = self.createFolder(OD_FOLDER)
+        self.document.update({'folder_id': self.folder_id})
+        logger.info('OneDrive folder ID cached')
 
     def _request(self, method, url, headers=None, **kwargs):
         # make a request with access token
         _header = {
             'Accept': 'application/json',
-            'Authorization': 'Bearer ' + self.token,
+            'Authorization': 'Bearer ' + self.access_token,
             'Content-Type': 'image/jpeg'
         }
         if headers:
             _header.update(headers)
         if 'files' not in kwargs:
             _header['Content-Type'] = 'application/json'
-        return self._parse(requests.request(method, url, headers=_header, **kwargs))
+        response = requests.request(method, url, headers=_header, **kwargs)
+        response.raise_for_status()
+        return self._parse(response)
 
     def _parse(self, response):
-        # stylish response
         if 'application/json' in response.headers['Content-Type']:
             r = response.json()
         else:
