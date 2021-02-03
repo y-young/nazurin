276e9b920a50261758778598760b66e9d96f6148
Googleplex <yyoung2001@gmail.com>
2021-02-03 22:35:55 +0800

    refactor: switch to motor

--- nazurin/database/mongo.py
+++ nazurin/database/mongo.py
@@ -1,4 +1,6 @@
-from pymongo import MongoClient
+from typing import Optional, Union
+
+from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import DuplicateKeyError
 
 from nazurin.config import env
@@ -9,40 +11,39 @@ class Mongo(object):
     def __init__(self):
         """Load credentials and initialize client."""
         URI = env.str('MONGO_URI', default='mongodb://localhost:27017/nazurin')
-        self.client = MongoClient(URI)
+        self.client = AsyncIOMotorClient(URI)
         self.db = self.client.get_default_database()
 
-    def collection(self, key):
+    def collection(self, key: str):
         self._collection = self.db[key]
         return self
 
-    def document(self, key):
+    def document(self, key: Union[str, int]):
         self._document = key
         return self
 
-    def get(self):
-        return self._collection.find_one({'_id': self._document})
+    async def get(self) -> Optional[dict]:
+        return await self._collection.find_one({'_id': self._document})
 
-    def exists(self):
-        return self._collection.count_documents({'_id': self._document},
-                                                limit=1) > 0
+    async def exists(self) -> bool:
+        count = await self._collection.count_documents({'_id': self._document},
+                                                       limit=1)
+        return count > 0
 
-    def insert(self, key, data):
+    async def insert(self, key: Optional[Union[str, int]], data: dict) -> bool:
         if key:
             data['_id'] = key
         try:
-            return self._collection.insert_one(data).acknowledged
-        except DuplicateKeyError:
-            raise NazurinError('Already exists in database.')
+            result = await self._collection.insert_one(data)
+            return result.acknowledged
+        except DuplicateKeyError as error:
+            raise NazurinError('Already exists in database.') from error
 
-    def update(self, data):
-        return self._collection.update_one({
-            '_id': self._document
-        }, {
-            '$set': data
-        }).modified_count == 1
+    async def update(self, data: dict) -> bool:
+        result = await self._collection.update_one({'_id': self._document},
+                                                   {'$set': data})
+        return result.modified_count == 1
 
-    def delete(self):
-        return self._collection.delete_one({
-            '_id': self._document
-        }).deleted_count == 1
+    async def delete(self) -> bool:
+        result = await self._collection.delete_one({'_id': self._document})
+        return result.deleted_count == 1
