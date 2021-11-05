5a86b945cb6ce37aaddd4bdd88a03ec8f8ab57fc
Yu Jin <im@yjin.dev>
2021-11-04 23:22:54 -0700

    feat: ArtStation support (#28)
    * feat($sites): added ArtStation support
    
    * fix($site:artstation): fixed filename with missing info
    
    * fix($site:artstation): fixed PR review requested changes

---
+++ nazurin/sites/Artstation/api.py
@@ -0,0 +1,75 @@
+import os
+from typing import List, Tuple
+
+from nazurin.models import Caption, Illust, Image
+from nazurin.utils import Request
+from nazurin.utils.exceptions import NazurinError
+
+class Artstation(object):
+    async def getPost(self, post_id: str):
+        """Fetch an post."""
+        api = f"https://www.artstation.com/projects/{post_id}.json"
+        async with Request() as request:
+            async with request.get(api) as response:
+                if not response.status == 200:
+                    raise NazurinError('Post not found')
+                post = await response.json()
+                return post
+
+    async def fetch(self, post_id: str) -> Illust:
+        post = await self.getPost(post_id)
+        imgs = self.getImages(post)
+        caption = self.buildCaption(post)
+        return Illust(imgs, caption, post)
+
+    def getImages(self, post) -> List[Image]:
+        """Get images from post."""
+        if 'assets' not in post.keys():
+            raise NazurinError('No asset found.')
+        assets = sorted(post['assets'], key=lambda x: x['position'])
+        hash_id = post['hash_id']
+        imgs = list()
+        for asset in assets:
+            if asset['asset_type'] != 'image':
+                continue
+            # https://cdnb.artstation.com/p/assets/images/images/042/908/363/large/_z-ed_-da.jpg?1635784439
+            filename, url, thumbnail = self.parseUrl(asset['image_url'])
+            imgs.append(
+                Image(f"ArtStation - {hash_id} - {filename}",
+                      url,
+                      thumbnail,
+                      width=asset['width'],
+                      height=asset['height']))
+        return imgs
+
+    def buildCaption(self, post) -> Caption:
+        user = post['user']
+        tags = post['tags']
+        tag_string = str()
+        for tag in tags:
+            tag_string += '#' + tag + ' '
+        return Caption({
+            'title': post['title'],
+            'author': f"{user['full_name']} #{user['username']}",
+            'url': f"https://www.artstation.com/artwork/{post['hash_id']}",
+            'tags': tag_string,
+        })
+
+    def parseUrl(self, src: str) -> Tuple[str, str, str]:
+        """Get filename, original file url & thumbnail url of the original image
+
+        eg:
+
+            src: 'https://cdnb.artstation.com/p/assets/images/images/042/908/363/large/_z-ed_-da.jpg?1635784439'
+
+            return:
+                '_z-ed_-da.jpg',
+                'https://cdnb.artstation.com/p/assets/images/images/042/908/363/4k/_z-ed_-da.jpg',
+                'https://cdnb.artstation.com/p/assets/images/images/042/908/363/medium/_z-ed_-da.jpg'
+
+        """
+        baseurl = src.split('?')[0]
+        basename = os.path.basename(baseurl)
+        url = baseurl.replace('/large/', '/4k/')
+        thumbnail = baseurl.replace('/large/', '/medium/')
+        return basename, url, thumbnail
---
+++ nazurin/sites/Artstation/interface.py
@@ -0,0 +1,23 @@
+from time import time
+
+from nazurin.database import Database
+from nazurin.models import Illust
+
+from .api import Artstation
+from .config import COLLECTION
+
+patterns = [
+    # https://www.artstation.com/artwork/2x3LaB
+    # https://catzz.artstation.com/projects/A9ELeq
+    r'artstation\.com/(?:artwork|projects)/([0-9a-zA-Z]+)'
+]
+
+async def handle(match, **kwargs) -> Illust:
+    post_id = match.group(1)
+    db = Database().driver()
+    collection = db.collection(COLLECTION)
+
+    illust = await Artstation().fetch(post_id)
+    illust.metadata['collected_at'] = time()
+    await collection.insert(post_id, illust.metadata)
+    return illust
