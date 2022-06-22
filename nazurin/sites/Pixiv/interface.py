3609f3306cc0327d0162f54c210a8e7a201c3f58
yyoung <yyoung2001@gmail.com>
2022-06-22 11:17:44 +0800

    style: fix lines too long

--- nazurin/sites/Pixiv/interface.py
+++ nazurin/sites/Pixiv/interface.py
@@ -11,7 +11,8 @@ patterns = [
     # https://pixiv.net/artworks/123456
     # https://www.pixiv.net/en/artworks/123456
     # https://www.pixiv.net/member_illust.php?mode=medium&illust_id=123456
-    r'(?:www\.)?pixiv\.net/(?:en/)?(?:(?:i|artworks)/|member_illust\.php\?(?:mode=[a-z_]*&)?illust_id=)(\d+)',
+    r'(?:www\.)?pixiv\.net/(?:en/)?'\
+    r'(?:(?:i|artworks)/|member_illust\.php\?(?:mode=[a-z_]*&)?illust_id=)(\d+)',
 
     # https://i.pximg.net/img-original/img/2020/02/02/20/00/02/123456_p0.png
     # https://i1.pixiv.net/img-original/img/2020/02/02/20/00/02/123456_p0.png
@@ -19,7 +20,8 @@ patterns = [
     # https://i.pximg.net/img-master/img/2020/02/02/20/00/02/123456_p0_master1200.jpg
     # https://i1.pixiv.net/img-original/img/2020/02/02/20/00/02/123456_ugoira1920x1080.zip
     # https://i.pximg.net/c/540x540_10_webp/img-master/img/2020/02/02/20/00/02/123456_p0_square1200.jpg
-    r'[^./]+\.(?:pximg|pixiv)\.net/(?:\w+/)*img-[a-z-]+/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/(\d+)',
+    r'[^./]+\.(?:pximg|pixiv)\.net/'\
+    r'(?:\w+/)*img-[a-z-]+/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/(\d+)',
 
     # http://img1.pixiv.net/img/abcdef/123456.jpg
     # http://img1.pixiv.net/img/abcdef/123456_s.jpg
