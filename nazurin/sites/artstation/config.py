719ce6008f8cf0fdcac53fc9bad3cbad0c63881b
yyoung <yyoung2001@gmail.com>
2022-11-03 22:28:30 +0800

    feat: customize Artstation storage path & filename

--- nazurin/sites/artstation/config.py
+++ nazurin/sites/artstation/config.py
@@ -6,3 +6,5 @@ COLLECTION = 'artstation'
 with env.prefixed('ARTSTATION_'):
     with env.prefixed('FILE_'):
         DESTINATION: str = env.str('PATH', default='Artstation')
+        FILENAME: str = env.str('NAME',
+                                default='{title} ({hash_id}) - {filename}')
