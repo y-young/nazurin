8d74c61301b012e720beb851a9417f2e8f9dea01
yyoung <yyoung2001@gmail.com>
2022-11-04 22:57:44 +0800

    feat: customize Twitter storage path & filename

--- nazurin/sites/twitter/config.py
+++ nazurin/sites/twitter/config.py
@@ -6,3 +6,5 @@ COLLECTION = 'twitter'
 with env.prefixed('TWITTER_'):
     with env.prefixed('FILE_'):
         DESTINATION: str = env.str('PATH', default='Twitter')
+        FILENAME: str = env.str(
+            'NAME', default='{id_str}_{index} - {user[name]}({user[id_str]})')
