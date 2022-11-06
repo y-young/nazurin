557a335ccaf250f360d4fec716be2c933f8f32f0
yyoung <yyoung2001@gmail.com>
2022-11-06 16:06:34 +0800

    feat: customize Bilibili storage path & filename

--- nazurin/sites/bilibili/config.py
+++ nazurin/sites/bilibili/config.py
@@ -6,3 +6,6 @@ COLLECTION = 'bilibili'
 with env.prefixed('BILIBILI_'):
     with env.prefixed('FILE_'):
         DESTINATION: str = env.str('PATH', default='Bilibili')
+        FILENAME: str = env.str(
+            'NAME',
+            default='{dynamic_id_str}_{index} - {user[name]}({user[uid]})')
