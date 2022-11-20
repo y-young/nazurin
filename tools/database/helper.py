7cb7a33f891ce745d0e39b7380c431ccf3d23cf6
yyoung <yyoung2001@gmail.com>
2022-11-20 17:07:14 +0800

    fix: tool scripts

--- tools/database/helper.py
+++ tools/database/helper.py
@@ -1,27 +1,38 @@
+# pylint: disable=import-error, wrong-import-position, global-statement
+"""
+Helper script to scan artworks from local directory and write metadata to database.
+Run this script in the root directory of the project.
+"""
+
+import glob
+import logging
 import os
+import random
 import re
 import sys
-import glob
-import random
-import logging
-from time import time, sleep
+from time import sleep, time
 
-sys.path.append('../../')
-from database import Database
-from sites import SiteManager
-from sites.Moebooru.config import COLLECTIONS as MOEBOORU_COLLECTIONS
+sys.path.append('.')
+from nazurin.database import Database
+from nazurin.sites import SiteManager
+from nazurin.sites.moebooru.config import COLLECTIONS as MOEBOORU_COLLECTIONS
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 logger = logging.getLogger('helper')
 sites = SiteManager()
 
 success = file_cnt = artworks = 0
-no_match = list()
+no_match = []
 directory = ''
-error = list()
+error = []
 processed = {}
 sites.load()
-SITES = ['pixiv', 'danbooru', 'yandere', 'konachan', 'lolibooru', 'zerochan', 'twitter', 'bilibili']
+SITES = [
+    'pixiv', 'danbooru', 'yandere', 'konachan', 'lolibooru', 'zerochan',
+    'twitter', 'bilibili'
+]
 
 def scan():
     patterns = {
@@ -33,12 +44,13 @@ def scan():
         'danbooru_new': r'^danbooru (\d+)',
         'zerochan': r'^Zerochan (\d+)'
     }
-    global file_cnt, no_match, directory
+    global file_cnt
     files = glob.glob(directory + '*')
     for path in files:
         file_cnt = file_cnt + 1
         filename = path[len(directory):]
         flag = False
+        source = None
 
         for source, pattern in patterns.items():
             match = re.search(pattern, filename, re.IGNORECASE)
@@ -47,11 +59,11 @@ def scan():
                 break
         if not flag:
             no_match.append(filename)
-            logger.warning(filename + ': ' + "No match")
+            logger.warning('%s: No match', filename)
             continue
         yield (filename, source, match)
 
-def parseSource(source, match):
+def parse_source(source, match):
     collection = source
     origin_id = match.group(1)
     if source == 'moebooru':
@@ -70,12 +82,12 @@ def process(filename, source, match, origin_id):
     api = sites.api(source)
     if source == 'pixiv':
         data = api.getArtwork(origin_id)
-        sleep(random.random()+0.5) # Rate limit
+        sleep(random.random() + 0.5)  # Rate limit
     elif source == 'twitter':
         data = api.getTweet(origin_id)
     elif source == 'danbooru':
         data = api.getPost(origin_id)
-        if not filename.startswith('danbooru'): # Rename old format to new
+        if not filename.startswith('danbooru'):  # Rename old format to new
             _, newname = api._getNames(data)
             os.rename(directory + filename, directory + newname)
     elif source == 'moebooru':
@@ -90,43 +102,46 @@ def process(filename, source, match, origin_id):
         data = api.getDynamic(origin_id)
     return data
 
-def printResult():
-    logger.info("Scanned Files: %d, Matched Artworks: %d, Processed: %d, No Match: %d, Error: %d", file_cnt, artworks, success, len(no_match), len(error))
-    for key in processed.keys():
-        logger.info("%s: processed %d", key, len(processed[key]))
+def print_result():
+    logger.info(
+        "Scanned Files: %d, Matched Artworks: %d, Processed: %d, No Match: %d, Error: %d",
+        file_cnt, artworks, success, len(no_match), len(error))
+    for key, item in processed.items():
+        logger.info("%s: processed %d", key, len(item))
     if no_match:
         logger.info("⚠️ No match:\n %s", no_match)
     if error:
         logger.info("❌ Error:\n %s", error)
 
 def main():
-    global artworks, success, directory, error, processed
+    global artworks, success, directory
     for source in SITES:
-        processed[source] = list()
+        processed[source] = []
     directory = input('Directory path (ends with slash): ')
     print('Skip sites(enter site name in SITES, separated with comma):')
-    print('SITES='+str(SITES))
+    print('SITES=' + str(SITES))
     skipped = input()
     skipped = skipped.split(',')
     db = Database().driver()
 
     for filename, source, match in scan():
-        origin_id, site = parseSource(source, match)
+        origin_id, site = parse_source(source, match)
         if source == 'danbooru_new':
             source = site = 'danbooru'
         if site in skipped:
             continue
 
-        if origin_id in processed[site]: # Pixiv multiple pages
+        if origin_id in processed[site]:  # Pixiv multiple pages
             continue
         artworks = artworks + 1
         if db.collection(site).document(origin_id).exists():
-            logger.info('⚪️ ' + filename + ': Already exists')
+            logger.info('⚪️ %s: Already exists', filename)
             processed[site].append(origin_id)
             continue
 
         try:
             data = process(filename, source, match, origin_id)
+        # pylint: disable=broad-except
         except Exception as err:
             logger.error("❌ %s: (id: %s) %s", filename, origin_id, err)
             error.append((filename, origin_id, err))
@@ -135,15 +150,15 @@ def main():
 
         data['collected_at'] = time()
         db.collection(site).insert(origin_id, data)
-        logger.info('✔️ ' + filename + ': ' + source + str(match.groups()))
+        logger.info('✔️ %s: %s%s', filename, source, str(match.groups()))
 
         processed[site].append(origin_id)
         success = success + 1
 
-    printResult()
+    print_result()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        printResult()
+        print_result()
