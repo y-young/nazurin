fcb611342821564d51371ca629742f9621bd2c20
Kare-Udon <52364111+Kare-Udon@users.noreply.github.com>
2023-09-26 21:20:00 +0800

    feat: add support for S3 compatible storage (#84)
    * Add: support for S3 storage
    
    * Add: S3 relevant documents
    
    * Update: add content-type for file upload option
    
    * Update: concatenate dest and name to build path
    
    * Fix: wrong document path
    
    * Fix: remove unused encoding heading
    
    * Update: rename envs
    
    * Update: turn sync funcs to async ones
    
    * Update: change S3 Region default to None
    
    * Fix: duplicated async call
    
    * Fix: cross-platform path construction
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ nazurin/storage/s3.py
@@ -0,0 +1,55 @@
+import asyncio
+import mimetypes
+import pathlib
+from typing import List
+
+from minio import Minio
+
+from nazurin.config import env
+from nazurin.models import File
+from nazurin.utils import logger
+from nazurin.utils.decorators import async_wrap
+
+with env.prefixed("S3_"):
+    ENDPOINT = env.str("ENDPOINT", default="s3.amazonaws.com")
+    ACCESS_KEY = env.str("ACCESS_KEY")
+    SECRET_KEY = env.str("SECRET_KEY")
+    SECURE = env.bool("SECURE", default=True)
+    REGION = env.str("REGION", default=None)
+    BUCKET = env.str("BUCKET", default="nazurin")
+
+
+class S3:
+    """S3 driver"""
+
+    client = Minio(
+        endpoint=ENDPOINT,
+        access_key=ACCESS_KEY,
+        secret_key=SECRET_KEY,
+        region=REGION,
+        secure=SECURE,
+    )
+
+    @async_wrap
+    def check_bucket(self):
+        if not S3.client.bucket_exists(BUCKET):
+            S3.client.make_bucket(BUCKET)
+            logger.info("Bucket created: {}", BUCKET)
+
+    @async_wrap
+    def upload(self, file: File):
+        S3.client.fput_object(
+            bucket_name=BUCKET,
+            object_name=pathlib.Path(file.destination, file.name).as_posix(),
+            file_path=file.path,
+            content_type=mimetypes.guess_type(file.name)[0]
+            or "application/octet-stream",
+        )
+
+    async def store(self, files: List[File]):
+        await self.check_bucket()
+
+        tasks = [self.upload(file) for file in files]
+        await asyncio.gather(*tasks)
+
+        return True
