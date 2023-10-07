43b05ecb891ff05028bdc6225aa84e1aca890187
yyoung <yyoung2001@gmail.com>
2023-10-07 20:19:24 +0800

    feat: add kemono.party support

---
+++ nazurin/sites/kemono/config.py
@@ -0,0 +1,11 @@
+from nazurin.config import env
+
+PRIORITY = 10
+COLLECTION = "kemono"
+
+with env.prefixed("KEMONO_"):
+    with env.prefixed("FILE_"):
+        DESTINATION: str = env.str(
+            "PATH", default="Kemono/{service}/{username} ({user})/{title} ({id})"
+        )
+        FILENAME: str = env.str("NAME", default="{pretty_name}")
