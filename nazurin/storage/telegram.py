733af00d472bc35d545595e388e7778ebf17ba0c
yyoung <yyoung2001@gmail.com>
2023-11-12 18:42:39 +0800

    fix: skip files larger than 50MB when saving to TG

--- nazurin/storage/telegram.py
+++ nazurin/storage/telegram.py
@@ -4,12 +4,25 @@ from typing import List
 from nazurin import bot
 from nazurin.config import env
 from nazurin.models import File
+from nazurin.utils import logger
 
 ALBUM_ID = env.int("ALBUM_ID")
 
 
 class Telegram:
     async def store(self, files: List[File]):
-        tasks = [bot.send_doc(file, chat_id=ALBUM_ID) for file in files]
+        tasks = []
+        for file in files:
+            size = await file.size()
+            if size > 50 * 1024 * 1024:
+                # Telegram bot currently only supports files up to 50MB
+                # https://core.telegram.org/bots/api#senddocument
+                logger.warning(
+                    "File {} exceeds size limit ({} bytes) and won't be save to Telegram",
+                    file.name,
+                    size,
+                )
+                continue
+            tasks.append(bot.send_doc(file, chat_id=ALBUM_ID))
         await asyncio.gather(*tasks)
         return True
