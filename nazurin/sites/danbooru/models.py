bf916bfa20c0af16ff49c5ea2cfc8dcb51be64bf
yyoung <yyoung2001@gmail.com>
2024-01-13 21:21:08 +0800

    fix: Danbooru download still hits CF check

--- nazurin/sites/danbooru/models.py
+++ nazurin/sites/danbooru/models.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 
 from nazurin.models import Illust
-from nazurin.utils.network import CurlRequest
+from nazurin.utils.network import CloudScraperRequest
 
 
 @dataclass
 class DanbooruIllust(Illust):
     async def download(self, **kwargs):
-        await super().download(request_class=CurlRequest, **kwargs)
+        await super().download(request_class=CloudScraperRequest, **kwargs)
--- nazurin/utils/network.py
+++ nazurin/utils/network.py
@@ -4,11 +4,14 @@ from contextlib import AbstractAsyncContextManager, asynccontextmanager
 from typing import AsyncContextManager, Generator, Optional, Union
 
 import aiofiles
+import cloudscraper
 from aiohttp import ClientSession, ClientTimeout, TCPConnector
 from curl_cffi.requests import AsyncSession as CurlSession
 from curl_cffi.requests import Response as CurlResponse
+from requests import Session
 
 from nazurin.config import DOWNLOAD_CHUNK_SIZE, PROXY, TIMEOUT, UA
+from nazurin.utils.decorators import async_wrap
 from nazurin.utils.logging import logger
 
 
@@ -116,3 +119,46 @@ class CurlRequest(CurlSession, NazurinRequestSession):
             async with aiofiles.open(destination, "wb") as f:
                 async for chunk in response.aiter_content():
                     await f.write(chunk)
+
+
+class CloudScraperRequest(NazurinRequestSession):
+    """
+    Wrapped curl_cffi AsyncSession to impersonate a browser,
+    with timeout and proxy support.
+    """
+
+    def __init__(
+        self,
+        cookies: Optional[dict] = None,
+        headers: Optional[dict] = None,
+        timeout: int = TIMEOUT,
+        **kwargs
+    ):
+        proxies = {"https": PROXY, "http": PROXY} if PROXY else {}
+        session = Session()
+        session.cookies.update(cookies or {})
+        session.headers.update(headers or {})
+        session.proxies.update(proxies or {})
+        self.timeout = timeout
+        self.scraper = cloudscraper.create_scraper(sess=session, **kwargs)
+
+    @asynccontextmanager
+    async def get(
+        self, *args, **kwargs
+    ) -> Generator[cloudscraper.requests.Response, None, None]:
+        yield await async_wrap(self.scraper.get)(*args, timeout=self.timeout, **kwargs)
+
+    async def download(self, url: str, destination: Union[str, os.PathLike]):
+        async with self.get(url, stream=True) as response:
+            if not response.ok:
+                logger.error(
+                    "Download failed with status code {}", response.status_code
+                )
+                logger.info("Response: {}", await response.text)
+                response.raise_for_status()
+            async with aiofiles.open(destination, "wb") as f:
+                for chunk in response.iter_content(DOWNLOAD_CHUNK_SIZE):
+                    await f.write(chunk)
+
+    async def __aexit__(self, *args, **kwargs):
+        self.scraper.close()
