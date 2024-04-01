d486712d9aa9b5934cd42ec5e5504298621f2b8b
OriLight <61224409+orilights@users.noreply.github.com>
2024-04-01 22:57:18 +0800

    feat: save kemono post revision (#96)
    * feat: add kemono revision support
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * refactor: API and interface
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: yyoung <yyoung2001@gmail.com>

--- nazurin/sites/kemono/api.py
+++ nazurin/sites/kemono/api.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timezone
 from mimetypes import guess_type
-from typing import Tuple
+from typing import ClassVar, Tuple, Union
 
 from bs4 import BeautifulSoup
 
@@ -15,16 +15,39 @@ from .config import DESTINATION, FILENAME
 
 
 class Kemono:
+    API_BASE: ClassVar[str] = "https://kemono.su/api/v1"
+
     @network_retry
     async def get_post(self, service: str, user_id: str, post_id: str) -> dict:
         """Fetch an post."""
-        api = f"https://kemono.su/api/v1/{service}/user/{user_id}/post/{post_id}"
+        api = f"{self.API_BASE}/{service}/user/{user_id}/post/{post_id}"
         async with Request() as request:
             async with request.get(api) as response:
                 response.raise_for_status()
                 post = await response.json()
                 if not post:
                     raise NazurinError("Post not found")
+                username = await self.get_username(service, user_id)
+                post["username"] = username
+                return post
+
+    @network_retry
+    async def get_post_revision(
+        self, service: str, user_id: str, post_id: str, revision_id: str
+    ) -> dict:
+        """Fetch a post revision."""
+        api = f"{self.API_BASE}/{service}/user/{user_id}/post/{post_id}/revisions"
+        async with Request() as request:
+            async with request.get(api) as response:
+                response.raise_for_status()
+                revisions = await response.json()
+                post = None
+                for revision in revisions:
+                    if str(revision["revision_id"]) == revision_id:
+                        post = revision
+                        break
+                if not post:
+                    raise NazurinError("Post revision not found")
                 username = await self.get_username(service, user_id)
                 post["username"] = username
                 return post
@@ -43,8 +66,13 @@ class Kemono:
                 username = tag.get("content", "")
                 return username
 
-    async def fetch(self, service: str, user_id: str, post_id: str) -> Illust:
-        post = await self.get_post(service, user_id, post_id)
+    async def fetch(
+        self, service: str, user_id: str, post_id: str, revision_id: Union[str, None]
+    ) -> Illust:
+        if revision_id:
+            post = await self.get_post_revision(service, user_id, post_id, revision_id)
+        else:
+            post = await self.get_post(service, user_id, post_id)
         caption = self.build_caption(post)
 
         images = []
@@ -114,15 +142,23 @@ class Kemono:
         return (DESTINATION.format_map(context), filename + extension)
 
     @staticmethod
+    def get_url(post: dict) -> str:
+        url = (
+            f"https://kemono.su/{post['service']}"
+            f"/user/{post['user']}/post/{post['id']}"
+        )
+        revision = post.get("revision_id")
+        if revision:
+            url += f"/revision/{post['revision_id']}"
+        return url
+
+    @staticmethod
     def build_caption(post) -> Caption:
         return Caption(
             {
                 "title": post["title"],
                 "author": "#" + post["username"],
-                "url": (
-                    f"https://kemono.su/{post['service']}"
-                    f"/user/{post['user']}/post/{post['id']}"
-                ),
+                "url": Kemono.get_url(post),
             }
         )
 
