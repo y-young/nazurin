854ff1f0f66aaf19a4ae28bca41b5946fd8b9def
yyoung <yyoung2001@gmail.com>
2025-08-23 11:13:46 +0800

    fix: ruff check

--- nazurin/database/__init__.py
+++ nazurin/database/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import importlib
-from typing import Callable, Optional, Union
+from typing import Callable, Union
 
 from nazurin.config import DATABASE
 
@@ -24,13 +24,13 @@ class DatabaseDriver:
     def document(self, key: Union[str, int]) -> DatabaseDriver:
         raise NotImplementedError
 
-    async def get(self) -> Optional[dict]:
+    async def get(self) -> dict | None:
         raise NotImplementedError
 
     async def exists(self) -> bool:
         raise NotImplementedError
 
-    async def insert(self, key: Optional[Union[str, int]], data: dict) -> bool:
+    async def insert(self, key: Union[str, int] | None, data: dict) -> bool:
         raise NotImplementedError
 
     async def update(self, data: dict) -> bool:
